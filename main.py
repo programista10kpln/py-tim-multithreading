2fc0f621761762364e71ec340e6606f20559d463
programista10kpln <programista10kpln@proton.me>
2022-08-30 13:00:43 +0200

    checkout simulation

--- main.py
+++ main.py
@@ -11,9 +11,26 @@ class MyThread(threading.Thread):
 
     def run(self):
         print(f'Starting {self.name} \n')
+        # only this thread can be active, rest is locked
         threadLock.acquire()
         print_time(self.name, 1, self.count)
         threadLock.release()
+        print(f'Exiting {self.name} \n')
+
+
+class MyThread2(threading.Thread):
+    def __init__(self, threadId, name, count):
+        threading.Thread.__init__(self)
+        self.threadId = threadId
+        self.name = name
+        self.count = count
+
+    def run(self):
+        print(f'Starting {self.name} \n')
+        # threads of this class can work at the same time
+        threadLock.acquire()
+        threadLock.release()
+        print_time(self.name, 1, self.count)
         print(f'Exiting {self.name} \n')
 
 
@@ -27,11 +44,15 @@ def print_time(name, delay, count):
 # run thread2 after thread1 finishes its work
 threadLock = threading.Lock()
 
-thread1 = MyThread(1, "Thread1", 10)
-thread2 = MyThread(2, "Thread2", 5)
+# after thread1 finishes the rest cant work at the same time
+thread1 = MyThread(1, "Payment", 5)
+thread2 = MyThread2(2, "Sending email", 10)
+thread3 = MyThread2(3, "Loading page", 3)
 
 thread1.start()
 thread2.start()
+thread3.start()
 thread1.join()
 thread2.join()
+thread3.join()
 print("Done main thread")
